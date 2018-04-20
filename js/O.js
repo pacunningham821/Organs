2ef8aa8fbd59885c30dd25f8645629ef6d70cfba
Cunningham <pacunningham821@gmail.com>
2018-04-20 10:38:46 -0400

    off GE network

--- js/O.js
+++ js/O.js
@@ -16,6 +16,7 @@ var H;
 var W;
 var X;
 var Y;
+var ID;
 
 d3.csv("https://raw.githubusercontent.com/pacunningham821/Organs/master/2017_All.csv", function(data){
 
@@ -27,17 +28,22 @@ d3.csv("https://raw.githubusercontent.com/pacunningham821/Organs/master/2017_All
 		.attr("width", DaVal)
 		.attr("height", DaVal)
 		.attr("fill", d3.rgb(data.R,data.G,data.B))
+		.attr("stroke-width", 0.5)
+		.attr("stroke", d3.rgb(80,80,80))
 		.attr("id", data.Organ)
+		.attr("rx", 20)
+		.attr("ry", 20)
 		.on("mouseover", handleMouseOver)
 		.on("mouseout", handleMouseOut);
 		
 		canvas.append("text")
 			.attr("x", 115)
 			.attr("y", 515 - DaVal)
-			.attr("fint-family", "Calibri")
+			.attr("font-family", "Calibri")
 			.attr("font-size", "20px")
 			.attr("fill", "white")
-			.attr("font-weight", 700)
+			.attr("font-weight", 400)
+			.attr("id", data.Organ + "TXT")
 			.text(data.Organ);
 		
 	i++;	
@@ -52,19 +58,51 @@ d3.csv("https://raw.githubusercontent.com/pacunningham821/Organs/master/2017_All
 			W = parseFloat(d3.select(this).attr("width"));
 			X = parseInt(d3.select(this).attr("x"));
 			Y = parseInt(d3.select(this).attr("y"));
+			ID = "#" + d3.select(this).attr("id") + "TXT";
 			
-            d3.select(this).attr("height", W + 40);
-			d3.select(this).attr("width", H + 40);
-			d3.select(this).attr("x", X - 20);
-			d3.select(this).attr("y", Y - 20);
+            d3.select(this).attr("height", W + 40)
+			.attr("width", H + 40)
+			.attr("x", X - 20)
+			.attr("y", Y - 20)
+			.attr("stroke-width", 2);
+			
+			d3.select(ID).attr("font-weight", 700);
+			
+			canvas.append("rect")
+			.attr("x", X - 25)
+			.attr("y", Y - 40)
+			.attr("fill", d3.rgb(145,145,145))
+			.attr("id", "Bub")
+			.attr("height", 15)
+			.attr("width", 45)
+			.attr("rx", 7.5)
+			.attr("ry", 7.5);
+			
+			canvas.append("text")
+			.attr("x", X - 19)
+			.attr("y", Y - 29)
+			.attr("font-family", "Calibri")
+			.attr("font-size", "14px")
+			.attr("fill", "white")
+			.attr("font-weight", 700)
+			.attr("id", "TXT")
+            .text(Math.round(Math.pow(W/2.5, 2)).toString());
+			
 
           }
 
       function handleMouseOut(d, i) {
-            d3.select(this).attr("height", H);
-			d3.select(this).attr("width", W);
-			d3.select(this).attr("x", X);
-			d3.select(this).attr("y", Y);
+            d3.select(this).attr("height", H)
+			.attr("width", W)
+			.attr("x", X)
+			.attr("y", Y)
+			.attr("stroke-width", 0.5);
+			
+			d3.select(ID).attr("font-weight", 400);
+			
+			d3.select("#Bub").remove();
+			
+			d3.select("#TXT").remove();
 
           }
 	
